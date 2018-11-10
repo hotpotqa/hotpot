34bea4e1399cbe94b396c4c2d0e7a4c8bdacceab
Zhilin Yang <kimiyoung@yeah.net>
2018-11-09 22:36:08 -0500

    Update download.sh

--- download.sh
+++ download.sh
@@ -2,7 +2,7 @@
 # Download Hotpot Data
 wget http://curtis.ml.cmu.edu/datasets/hotpot/hotpot_dev_distractor_v1.json
 wget http://curtis.ml.cmu.edu/datasets/hotpot/hotpot_dev_fullwiki_v1.json
-wget http://curtis.ml.cmu.edu/datasets/hotpot/hotpot_train_v1.json
+wget http://curtis.ml.cmu.edu/datasets/hotpot/hotpot_train_v1.1.json
 
 # Download GloVe
 GLOVE_DIR=./
