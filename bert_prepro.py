7322c219eba68afdaf43b97ce6f73c4f05f6fbd8
arinaruck <arina.ruck@gmail.com>
2020-12-11 12:01:40 +0100

    update

--- bert_prepro.py
+++ bert_prepro.py
@@ -3,12 +3,12 @@ from tqdm import tqdm
 import spacy
 import ujson as json
 import jsonlines
+import pickle
 import collections
 from collections import Counter
 import numpy as np
 import os.path
 import argparse
-import torch
 import torch
 import os
 from joblib import Parallel, delayed
@@ -19,6 +19,7 @@ import re
 import string
 import copy
 from transformers import BertTokenizer
+import shutil
 
 BERT_MAX_SEQ_LEN = 512
 tokenizer = BertTokenizer.from_pretrained('bert-large-cased', return_token_type_ids=True)
@@ -107,7 +108,7 @@ def remove_punctuation(sent):
     return re.sub('[%s]' % re.escape(string.punctuation), '', sent)
 
 def preprocess(sent):
-    return sent
+    return sent.encode('latin-1', 'ignore').decode('latin-1')
 
 def encode(question, context, max_length, config):
     """
@@ -171,22 +172,19 @@ def _process_article(article, config):
     is_answerable = True
     is_yes_no = False
     yes_no = None  # 0 if 'no', 1 otherwise
-    if 'answer' in article:
+    
+    if 'answer' in article.keys():
+        best_indices = [0, 0]
         answer = preprocess(article['answer'].strip())
-        if answer.lower() == 'yes':
-            best_indices = [-2, -2]
+        if answer.lower() in ['yes', 'no']:
             is_yes_no = True
-            yes_no = 1
-        elif answer.lower() == 'no':
-            best_indices = [-1, -1]
-            is_yes_no = True
-            yes_no = 1
+            answer = answer.lower()
+            yes_no = int(answer == 'yes')
         else:
             if answer not in text_context:
                 # in the fullwiki setting, the answer might not have been retrieved
                 # use (0, 1) so that we can proceed
                 answer = ''
-                best_indices = [0, 0]
                 is_answerable = False
             else:
                 best_indices, _ = fix_span(text_context, flat_offsets, answer)
@@ -214,6 +212,7 @@ def process_file(filename, config):
     outputs = Parallel(n_jobs=12, verbose=10)(delayed(_process_article)(article, config) for article in data)
     examples = [e[0] for e in outputs]
     eval_examples = [e[1] for e in outputs]
+
     random.shuffle(examples)
     print("{} questions in total".format(len(examples)))
 
@@ -267,6 +266,17 @@ def build_features(config, examples, data_type, out_file):
     doc_stride = config.doc_stride
     
     unique_id = 0
+
+    unanswerable_count = 0
+    yes_no_count = 0
+    span_count = 0
+    total_count = 0
+
+    unanswerable_ex_count = 0
+    yes_no_ex_count = 0
+    span_ex_count = 0
+    total_ex_count = len(examples)
+    max_chunks = 0
     for example in tqdm(examples):
         question_tokens = example['question_tokens']
         context_tokens = example['context_tokens']
@@ -295,11 +305,17 @@ def build_features(config, examples, data_type, out_file):
             if start_offset + length == len(context_tokens):
                 break
             start_offset += min(length, doc_stride)
-
+       
         is_answerable_example = example['is_answerable']
         is_yes_no_example = example['is_yes_no']
         yes_no_example = example['yes_no']
+
+        unanswerable_ex_count += (1 - is_answerable_example)
+        yes_no_ex_count += is_yes_no_example
+        span_ex_count += (1 - is_yes_no_example) * is_answerable_example
+
         supportive_facts = example['start_end_facts']
+        max_chunks = max(max_chunks, len(doc_spans))
         for (doc_span_index, doc_span) in enumerate(doc_spans):
             tokens = []
             segment_ids = []
@@ -347,10 +363,10 @@ def build_features(config, examples, data_type, out_file):
             doc_start = doc_span.start
             doc_end = doc_span.start + doc_span.length - 1
 
-            y1, y2 = None, None
+            y1, y2 = 0, 0
             is_answerable = 0
-            # yes -- 1, no -- 0
             is_yes_no = 0
+            # yes -- 1, no -- 0
             yes_no = 0
             answer_options = [1, 0]
             if config.is_training:
@@ -361,42 +377,105 @@ def build_features(config, examples, data_type, out_file):
                     y1 = start - doc_start + question_shift
                     y2 = end - doc_start + question_shift
                     is_answerable = 1
-                elif is_yes_no:
+                if is_yes_no_example:
+                    is_yes_no = 1
                     for s, e, sup in supportive_facts:
                         if sup and (s >= doc_start and e <= doc_end):
                             is_answerable = 1
-                            is_yes_no = 1
                             yes_no = answer_options[start]
                             y1, y2 = start, end
                             break
                     else:
                         y1, y2 = 0, 0
+                yes_no_count += is_yes_no
+                span_count += is_answerable * (1 - is_yes_no)
+                unanswerable_count += (1 - is_answerable)
+                total_count += 1
+            labels = [is_answerable, is_yes_no, yes_no, y1, y2]
             datapoints.append({'input_ids': input_ids, 'attention_mask': input_mask, 'token_type_ids': segment_ids,
-                               'max_context': max_context,
-                               'is_yes_no': is_yes_no, 'is_answerable': is_answerable, 'yes_no': yes_no,
-                               'y1': y1, 'y2': y2, 'example_id': example['id'], 'feature_id': unique_id})
+                               'max_context': max_context, 'feature_id': unique_id, 'example_id': example['id'],
+                               'labels' : labels})
             unique_id += 1
+
+    print(f'max chunks: {max_chunks}')
+    print(f"unanswerable: {unanswerable_count / total_count}, yes_no: {yes_no_count / total_count}, span: {span_count / total_count}")
+    print(f"unansweravle examples: {unanswerable_ex_count}, yes_no_examples: {yes_no_ex_count}, span: {span_ex_count}")
+
+    yes_no_examples = []
+    for datapoint in datapoints:
+        is_y_n = datapoint['labels'][1]
+        ex_id = datapoint['example_id']
+        if is_y_n:
+            yes_no_examples.append(ex_id)
+    print(f'YES NO EXAMPLES IN THE DATA: {len(set(yes_no_examples))}')
 
     dir_name = data_type
     try:
         os.mkdir(dir_name)
         print(f"Directory {dir_name} created")
     except OSError:
-        print(f"Directory {dir_name} already exists, writing files there")
-
+        print(f"Removing directory {dir_name} and creating a new one")
+        shutil.rmtree(dir_name)
+        os.mkdir(dir_name)
     fileparts = out_file.split('.')
     name, ext = '.'.join(fileparts[:-1]), fileparts[-1]
     num_objects = len(datapoints)
     num_files = config.num_files if config.num_files > 0 else num_objects
     batch_size = num_objects // num_files
-    for i in range(num_files - 1):
-        with jsonlines.open(f'{dir_name}/{name}_{str(i)}.{ext}', mode='w') as writer:
-            for datapoint in datapoints[i * batch_size: (i + 1) * batch_size]:
-                writer.write(datapoint)
-    with jsonlines.open(f'{dir_name}/{name}_{str(num_files - 1)}.{ext}', mode='w') as writer:
-        for datapoint in datapoints[(num_files - 1) * batch_size:]:
-            writer.write(datapoint)
+    st = 0
+    # dividing datapoints between mltiple files for using lazy dataloading with multiple workers
+    # files are supposed to be of approximately equal size, but we do not want to split examples between files
 
+    examples_total = 0
+    for i in range(num_files - 1):
+        examples = [] # holds starting and ending lines of the examples in the respective file
+        start = 0
+        ex_id = None
+        is_y_n = False
+        with jsonlines.open(f'{dir_name}/{name}_{str(i)}.{ext}', mode='w') as writer:
+            for dp_idx, datapoint in enumerate(datapoints[st: (i + 1) * batch_size]):
+                if ex_id and datapoint['example_id'] != ex_id:
+                    end = dp_idx
+                    if is_y_n:
+                        examples.append((start, end))
+                    start = end
+                ex_id = datapoint['example_id']
+                is_y_n = datapoint['labels'][1]
+                writer.write(datapoint)
+            last_ex_id = datapoints[(i + 1) * batch_size - 1]['example_id']
+            j = (i + 1) * batch_size
+            while datapoints[j]['example_id'] == last_ex_id:
+                writer.write(datapoints[j])
+                j += 1
+            if is_y_n:
+                examples.append((start, j - st))
+            st = j
+        examples_total += len(examples)
+        with open(f'{dir_name}/{name}_{str(i)}_meta.pickle', mode='wb') as fp:
+            pickle.dump(examples, fp)
+    
+    examples = [] # holds starting and ending lines of the examples in the respective file
+    start = 0
+    ex_id = None
+    is_y_n = False
+
+    with jsonlines.open(f'{dir_name}/{name}_{str(num_files - 1)}.{ext}', mode='w') as writer:
+        for dp_idx, datapoint in enumerate(datapoints[st:]):
+            if ex_id and datapoint['example_id'] != ex_id:
+                end = dp_idx
+                if is_y_n:
+                    examples.append((start, end))
+                start = end
+            ex_id = datapoint['example_id']
+            is_y_n = datapoint['labels'][1]
+            writer.write(datapoint)
+        end = dp_idx
+        if is_y_n:
+            examples.append((start, end))
+    examples_total += len(examples)
+    print(f'EXAMPLES IN THE DATASET: {examples_total}')
+    with open(f'{dir_name}/{name}_{str(num_files - 1)}_meta.pickle', mode='wb') as fp:
+        pickle.dump(examples, fp)
 
 def save(filename, obj, message=None):
     if message is not None:
@@ -409,6 +488,7 @@ def prepro(config):
     random.seed(13)
     examples, eval_examples = process_file(config.data_file, config)
 
+    print(len(examples), len(eval_examples))
     if config.data_split == 'train':
         record_file = config.train_record_file
         eval_file = config.train_eval_file
@@ -420,6 +500,5 @@ def prepro(config):
         eval_file = config.test_eval_file
 
     build_features(config, examples, config.data_split, record_file)
-    if config.data_split in ['dev', 'test']:
-        save(eval_file, eval_examples, message='{} eval'.format(config.data_split))
+    save(eval_file, eval_examples, message='{} eval'.format(config.data_split))
 
